1a1e05e892d407d10d14082e7144c0ffb5054b50
jenlu33 <nejul33@gmail.com>
2020-03-17 14:32:26 -0700

    split up session_form to login and signup form components

--- frontend/src/components/session/signup_container.js
+++ frontend/src/components/session/signup_container.js
@@ -1,12 +1,12 @@
 import { connect } from 'react-redux';
 import { signup } from '../../actions/session_actions';
-import SessionForm from './session_form';
+import SignupForm from './session_form';
 
 const mapStateToProps = (state) => ({
   formType: 'sign up'
 })
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   processForm: (user) => dispatch(sign_up(user)),
 })
 
---
+++ frontend/src/components/session/signup_form.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      username: "",
+      password: ""
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user)
+
+    this.state({
+      email: "",
+      username: "",
+      password: ""
+    })
+  }
+
+  update(field) {
+    return e => this.ListeningStateChangedEvent({
+      [field]: e.currentTarget.value
+    })
+  }
+
+  render() {
+    const { formType } = this.props;
+
+    const display = formType === 'log in' ? (
+      <div className="change-form">
+
+      </div>
+    ) : (
+        <div className="change-form">
+
+        </div>
+      )
+
+    return (
+      <div className="main-session-div">
+        <div className="main-session-container">
+
+          <div className="change-form-container">
+            {display}
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default withRouter(SessionForm);
