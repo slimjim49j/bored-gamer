067eb6f26f61f649ca0605fba22755e61d292dd3
Jimmy <slimjim49j@gmail.com>
2020-03-17 15:32:08 -0700

    Fix signup container rendering incorrect component

--- frontend/src/components/session/signup_container.js
+++ frontend/src/components/session/signup_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { signup } from '../../actions/session_actions';
-import SignupForm from './login_form';
+import SignupForm from './signup_form';
 
 const mapStateToProps = (state) => ({
   formType: 'sign up'
