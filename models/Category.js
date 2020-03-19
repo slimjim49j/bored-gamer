b6034595aee3b9fad3c86988ceb52094d425fe20
Jimmy <slimjim49j@gmail.com>
2020-03-19 10:15:02 -0700

    Delete category and mechanic model files

--- models/Category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const CategorySchema = new Schema({
-  category: String
-});
-
-module.exports = Category = mongoose.model("Category", CategorySchema);
