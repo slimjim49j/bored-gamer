b32562c46765da27afe267caaec4038aba7a4e80
naveen thota <helloitsnaveen@gmail.com>
2020-03-19 10:41:31 -0700

    clean header component code

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -4,10 +4,6 @@ import { withRouter } from 'react-router-dom';
 import '../../assets/stylesheets/header.css'
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     const { currentUser, logout } = this.props;
 
