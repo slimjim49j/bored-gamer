443e42be1460d4f84100c313ab8d8f062351c443
Jimmy <slimjim49j@gmail.com>
2020-03-19 20:49:22 -0700

    Clean up game show route

--- routes/api/games.js
+++ routes/api/games.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const ObjectId = require("mongodb").ObjectID;
-
 
 const Game = require("../../models/Game");
 
@@ -46,15 +44,10 @@ router.get('/mechanics', (req, res) => {
 });
 
 router.get('/:gameId', (req, res) => {
-    // const game = Game.find({ _id: new ObjectId(req.params.gameId) });
     Game.findOne({ _id: req.params.gameId }).then(game => {
-        console.log(game)
         if (game) return res.json(game);
         else return res.status(422).json({ game: "This game does not exist" });
     });
-
-    // console.log(game);
-    // console.log(new ObjectId(req.params.gameId));
 })
 
 module.exports = router;
