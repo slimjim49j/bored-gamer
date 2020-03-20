521fa8f904cbb30222f3aa5dc784f1acc3c80f14
Jimmy <slimjim49j@gmail.com>
2020-03-19 17:16:53 -0700

    Add game show backend route

--- routes/api/games.js
+++ routes/api/games.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const ObjectId = require("mongodb").ObjectID;
+
 
 const Game = require("../../models/Game");
 
@@ -42,5 +44,17 @@ router.get('/mechanics', (req, res) => {
         };
     })
 });
+
+router.get('/:gameId', (req, res) => {
+    // const game = Game.find({ _id: new ObjectId(req.params.gameId) });
+    Game.findOne({ _id: req.params.gameId }).then(game => {
+        console.log(game)
+        if (game) return res.json(game);
+        else return res.status(422).json({ game: "This game does not exist" });
+    });
+
+    // console.log(game);
+    // console.log(new ObjectId(req.params.gameId));
+})
 
 module.exports = router;
