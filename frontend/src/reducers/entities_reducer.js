c04524e30eb4c9a5cc8a693766c7a0fbf4e68de9
Jimmy <slimjim49j@gmail.com>
2020-03-22 15:55:07 -0700

    Add games reducer to entities reducer

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import users from './users_reducer';
+import games from './games_reducer';
 
 export default combineReducers({
-    users
+    users,
+    games,
 });
