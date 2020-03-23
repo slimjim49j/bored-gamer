b1b78c11502b94774df9715724a51e2240b6a31c
Jimmy <slimjim49j@gmail.com>
2020-03-22 18:18:12 -0700

    Make main page and game index reliant on redux games slice

--- frontend/src/components/main/main_page_container.js
+++ frontend/src/components/main/main_page_container.js
@@ -1,18 +1,20 @@
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import { resetPageNum } from "../../actions/session_actions";
+import { getInitialGames } from "../../actions/game_index_actions";
 
 import MainPage from './main_page';
 
 const mapStateToProps = state => ({
   loggedIn: state.session.isAuthenticated,
   currentUser: state.session.user,
+  pageNum: state.session.pageNum,
 })
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout()),
   resetPageNum: () => dispatch(resetPageNum()),
-
+  getInitialGames: (pageNum, categories, mechanics) => dispatch(getInitialGames(pageNum, categories, mechanics)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
