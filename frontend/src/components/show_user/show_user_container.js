5a7d3dc02ac55f780099178f27e9cccea569fd9e
jenlu33 <nejul33@gmail.com>
2020-04-16 16:04:13 -0700

    Attempt to fix node_modules

--- frontend/src/components/show_user/show_user_container.js
+++ frontend/src/components/show_user/show_user_container.js
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux';
 import ShowUser from './show_user';
 import { receiveCurrentUser } from '../../actions/session_actions';
+import { getDislikes } from '../../util/dislike_util';
 
 
 const mapStateToProps = (state) => ({
   user: state.session.user
 })
 
-// const mapDispatchToProps = dispatch => ({
-//   receiveUser: (currentUser) => dispatch(receiveCurrentUser(currentUser))
-// })
+const mapDispatchToProps = dispatch => ({
+  getUserGames: (userId) => getDislikes(userId)
+})
 
-export default connect(mapStateToProps, null)(ShowUser);
+export default connect(mapStateToProps, mapDispatchToProps)(ShowUser);
