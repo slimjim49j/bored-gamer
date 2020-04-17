d762108ef10f7602ddb780f9c61f4bbd3586a715
Jimmy <slimjim49j@gmail.com>
2020-04-16 17:09:34 -0700

    Create create like route

--- models/User.js
+++ models/User.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
-
-const Like = new Schema({
-    game_id: ObjectId,
-    dislike: Boolean,
-})
+const Like = require("./Like");
 
 const UserSchema = new Schema({
     username: {
